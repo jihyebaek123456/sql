cfdfa94938900bce0840c4526346aa1d67bcfd4a
DESKTOP-MLAJ9CK\PC-15 <bjh28712745@gmail.com>
2021-03-15 09:39:57 +0900

    SELECT 쿼리 기본 문법

---
+++ 2021_03_11.sql
@@ -0,0 +1,11 @@
+--데이터 조회 방법
+--SELECT : 조회하고자 하는 컬럼명(테이블에 있는 컬럼명)
+--         모든 컬럼 조회 시 *
+--FROM : 데이터를 조회할 테이블 명시
+
+SELECT empno, ename
+FROM EMP;
+
+--필수 용어
+--EMPNO : 직원 번호, ENAME : 직원 이름, JOB : 담당 업무, MGR : 상위 담당자
+--HIREDATE : 입사일, SAL : 급여, COMM : 상여금, DEPTNO : 부서 번호
